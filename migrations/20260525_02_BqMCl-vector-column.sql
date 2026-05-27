da3074f1d11eca6c6fd81d9cc52ef17ddbc07540
let <leticiacostaoa@gmail.com>
2026-05-26 21:51:01 -0300

    fix ci error

--- migrations/20260525_02_BqMCl-vector-column.sql
+++ migrations/20260525_02_BqMCl-vector-column.sql
@@ -1,4 +1,4 @@
 -- vector_column
 -- depends: 20260525_01_create_extension
 
-ALTER TABLE papers ADD COLUMN title_embeddings halfvec(128) NOT NULL;
+ALTER TABLE papers ADD COLUMN title_embeddings halfvec(128);
